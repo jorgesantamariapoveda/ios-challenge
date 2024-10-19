253f574da02829544108a793ae656c537f62dcfd
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-19 18:25:50 +0200

    Create AppDelegate+applyNavigationBarStyle

---
+++ Idealista/Idealista/App/AppDelegate+applyNavigationBarStyle.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate+applyNavigationBarStyle.swift
+//  Idealista
+//
+//  Created by Jorge on 19/10/24.
+//
+
+import UIKit
+
+extension AppDelegate {
+    
+    func applyNavigationBarStyle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.idealistaGreen
+        appearance.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.idealistaGrayBlack
+        ]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
+        UINavigationBar.appearance().tintColor = UIColor.idealistaPurple
+    }
+}
