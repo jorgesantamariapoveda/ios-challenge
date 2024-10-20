833212eedda68c7d4963a14f5a7804ff86feaf63
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-20 20:38:07 +0200

    Create Double+

---
+++ Idealista/Idealista/Model/Helpers/Extensions/Double+.swift
@@ -0,0 +1,21 @@
+//
+//  Double+.swift
+//  Idealista
+//
+//  Created by Jorge on 20/10/24.
+//
+
+import Foundation
+
+extension Double {
+    func formatToCurrencyWithoutCurrencySymbol() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        if let formattedValue = formatter.string(from: NSNumber(value: self)) {
+            return formattedValue
+        } else {
+            return "\(self)"
+        }
+    }
+}
