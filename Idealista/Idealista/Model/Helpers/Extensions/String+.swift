7ec18d2d99f16ac2e3b36a12346c00b24ed5fd45
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-20 14:28:09 +0200

    Create String+

---
+++ Idealista/Idealista/Model/Helpers/Extensions/String+.swift
@@ -0,0 +1,20 @@
+//
+//  String+.swift
+//  Idealista
+//
+//  Created by Jorge on 20/10/24.
+//
+
+import Foundation
+
+extension String {
+    func capitalizeFirstWord() -> String {
+        var words = self.components(separatedBy: " ")
+        
+        if let firstWord = words.first {
+            words[0] = firstWord.capitalized
+        }
+        
+        return words.joined(separator: " ")
+    }
+}
