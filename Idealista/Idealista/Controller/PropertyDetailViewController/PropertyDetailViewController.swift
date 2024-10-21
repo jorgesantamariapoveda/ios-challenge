b79708efc1b7449cc60d3b6dc9cfb266d08c4ad8
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-21 23:11:34 +0200

    Update PropertyDetailViewController: append UIScrollView (provisional)

--- Idealista/Idealista/Controller/PropertyDetailViewController/PropertyDetailViewController.swift
+++ Idealista/Idealista/Controller/PropertyDetailViewController/PropertyDetailViewController.swift
@@ -12,6 +12,26 @@ final class PropertyDetailViewController: UIViewController {
     private let representable: PropertyRepresentable
     private var getDetailPropertyUseCase: GetDetailPropertyUseCase
     
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     init(
         representable: PropertyRepresentable,
         getDetailPropertyUseCase: GetDetailPropertyUseCase = GetDetailPropertyUseCaseImpl()
@@ -32,6 +52,8 @@ final class PropertyDetailViewController: UIViewController {
         addSubviews()
         addConstraints()
         
+        setupSubviews()
+        
         view.backgroundColor = .white
         title = "Detalle"
         
@@ -39,15 +61,52 @@ final class PropertyDetailViewController: UIViewController {
     }
     
     private func addSubviews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(stackView)
     }
     
     private func addConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    private func setupSubviews() {
+        for i in 0..<50 {
+            let label = UILabel()
+            label.text = "Label \(i + 1)"
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textAlignment = .center
+            label.backgroundColor = .lightGray
+            
+            stackView.addArrangedSubview(label)
+        }
     }
     
     private func loadData() {
         Task {
             do {
                 let value = try await getDetailPropertyUseCase.execute()
+                
                 print(":: \(value)")
             } catch let error as HTTPClientError {
                 print(":: \(error)")
