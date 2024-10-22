72560a223268c76ca5fd00b241cd5abc874b1a1a
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-22 23:57:52 +0200

    Create NormalDetailLabelView

---
+++ Idealista/Idealista/View/Labels/NormalDetailLabelView.swift
@@ -0,0 +1,32 @@
+//
+//  NormalDetailLabelView.swift
+//  Idealista
+//
+//  Created by Jorge on 22/10/24.
+//
+
+import UIKit
+
+final class NormalDetailLabelView: UILabel {
+
+    required init() {
+        super.init(frame: .zero)
+        configureView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        textAlignment = .left
+        font = .preferredFont(forTextStyle: .footnote)
+        textColor = .black
+        numberOfLines = 0
+    }
+    
+    func set(text: String) {
+        self.text = text
+    }
+}
