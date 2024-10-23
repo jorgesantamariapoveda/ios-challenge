18b5c10d858e1996123d3e608f7983dd48ee129b
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2024-10-23 22:55:29 +0200

    Create ButtonDetailView

---
+++ Idealista/Idealista/View/SwiftUI/ButtonDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  ButtonDetailView.swift
+//  Idealista
+//
+//  Created by Jorge on 23/10/24.
+//
+
+import SwiftUI
+
+struct ButtonDetailView: View {
+    let title: String
+    var systemName: String? = nil
+    let action: () -> Void
+    
+    enum Constants {
+        static let size: CGFloat = 16
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            VStack {
+                if let systemName {
+                    Image(systemName: systemName)
+                        .resizable()
+                        .frame(width: Constants.size, height: Constants.size)
+                }
+                Text(title)
+                    .font(.callout)
+            }
+            .tint(.idealistaPurple)
+        }
+    }
+}
+
+#Preview {
+    HStack {
+        Spacer()
+        ButtonDetailView(title: "Text button") {
+            print("limit::")
+        }
+        Spacer()
+        ButtonDetailView(title: "Classic button", systemName: "heart") {
+            print("limit::")
+        }
+        Spacer()
+    }
+}
